35dc0dbdae87c9b76e74a6493d9d83ab1a454ac6
tjlinares <tjlinares23@gmail.com>
2019-10-07 21:51:44 -0400

    Updates

---
+++ precache-manifest.553a393da70f73f5587d7a10b2813f2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5189c851b876792bce6b4b595603cd",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5e4528c0bd6055af9729",
+    "url": "/robo-friends/static/css/main.97411668.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robo-friends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "5e4528c0bd6055af9729",
+    "url": "/robo-friends/static/js/main.9047451e.chunk.js"
+  },
+  {
+    "revision": "4495fefb041e0d2f6e17",
+    "url": "/robo-friends/static/js/runtime-main.7eea84bc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
