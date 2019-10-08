cdcaa36d0d6a34612ec82ea51381645dee6c0507
tjlinares <tjlinares23@gmail.com>
2019-10-07 21:52:37 -0400

    adding files fro github pages

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Tj Linares',
+        username: 'TJ',
+        email: 'tjlinares23@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Lany Totentino',
+        username: 'Belle',
+        email: 'lanybelle@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Ricky Deoleo',
+        username: 'Rick',
+        email: 'Ricky@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Gregory Tejada',
+        username: 'GT',
+        email: 'gregorytejada@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Hector David',
+        username: 'Gary',
+        email: 'garycastro@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Gabriel Pineda',
+        username: 'Pastor',
+        email: 'gabrielpineda@gmail.com'
+    }, 
+    {
+        id: 7,
+        name: 'Rolando Gomez',
+        username: 'Serio',
+        email: 'rolandogomez@gmail.com'
+    }, 
+    {
+        id: 8,
+        name: 'Michael Cruz',
+        username: 'Oreja',
+        email: 'michaelcruz@gmail.com'
+    }, 
+    {
+        id: 9,
+        name: 'Tania Urena',
+        username: 'Smart',
+        email: 'taniaurena@gmail.com'
+    }, 
+    {
+        id: 10,
+        name: 'Leonel Fernandez',
+        username: 'Leon',
+        email: 'leonelfernandez@gmail.com'
+    }, 
+]
